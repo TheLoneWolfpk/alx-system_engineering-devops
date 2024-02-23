c454b77da4cf31669598b54a6cc48e11672b0f59
Prince <prince.affum45@gmail.com>
2024-02-23 14:03:21 -0800

    task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-# Using Puppet, install flask from pip3
+# install puppet-lint -v 2.1.0
+
 package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+ensure   => '2.1.0',
+provider => 'pip3',
 }
